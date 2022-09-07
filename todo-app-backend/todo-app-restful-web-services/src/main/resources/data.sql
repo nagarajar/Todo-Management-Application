04193172b5c2ae4a4839c2e021e011df1089ac2f
Nagarajar <nagarajar70@gmail.com>
2022-09-07 08:28:47 +0530

    first commit on backend

---
+++ todo-app-backend/todo-app-restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into todo(id, username, description, target_date, is_done) values
+(1001,'in28minutes','Learn JPA', CURRENT_DATE(), false),
+(1002,'in28minutes','Learn hibernet', CURRENT_DATE(), false),
+(1003,'in28minutes','Learn spring', CURRENT_DATE(), false);
---
+++ todo-app-backend/todo-app-restful-web-services/src/test/java/com/todo/app/rest/webservices/TodoAppRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.app.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
